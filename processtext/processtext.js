02e182783e50feb04493f57a8acb431bc3138855
Angelena Marciano <angelena29@gmail.com>
2018-12-13 10:34:22 -0500

    initial commit

--- processtext/processtext.js
+++ processtext/processtext.js
@@ -5,24 +5,24 @@ function processText() {
     text=text.replace(/\./g,' '); //period to " "
     text=text.split(" ");
 
-    var list=[];
+    var array=[];
     for (var x in text){
         if(text[x]==""){
             delete text[x];
         }
-        else if(list.includes(text[x])){
+        else if(array.includes(text[x])){
 
-            var a = list.indexOf(text[x]);
-            list.splice(a+1, 1, list[a+1]+1);
+            var a = array.indexOf(text[x]);
+            array.splice(a+1, 1, array[a+1]+1);
 
         }
         else {
-            list.push(text[x]);
-            var a = list.indexOf(text[x]);
-            list.splice(a+1, 0, 1);
+            array.push(text[x]);
+            var a = array.indexOf(text[x]);
+            array.splice(a+1, 0, 1);
         }
 
     }
 
-    document.getElementById("output").innerHTML = list.join(" ");
+    document.getElementById("output").innerHTML = array.join(" ");
 }
